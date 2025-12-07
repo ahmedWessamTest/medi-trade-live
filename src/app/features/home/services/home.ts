0034685bda2324dd072b02033e09fe190034489a
ahmedWessamTest <ahmedwessam202@gmail.com>
2025-12-07 11:32:14 +0200

    return old version

--- src/app/features/home/services/home.ts
+++ src/app/features/home/services/home.ts
@@ -1,28 +1,14 @@
-import { inject, Injectable, makeStateKey, PLATFORM_ID, TransferState } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { API_CONFIG } from '@core/config/api-endpoints';
 import { Http } from '@core/services/api.service';
-import { Observable, of, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IHome } from '../interface/home';
-import { isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Home extends Http {
-  private transferState = inject(TransferState);
-  private _PLATFORM_ID = inject(PLATFORM_ID);
-  
   getHome(lang: string): Observable<IHome> {
-    const KEY = makeStateKey<any>('homeData');
-    if(this.transferState.hasKey(KEY)) {
-      const data = this.transferState.get(KEY,null);
-      this.transferState.remove(KEY);
-      return of(data);
-    }
-    return this.get<IHome>(`${API_CONFIG.BASE_URL}/${API_CONFIG.HOME}?lang=${lang}`).pipe(tap(data => {
-      if(isPlatformServer(PLATFORM_ID)){
-        this.transferState.set(KEY,data);
-      }
-    }));
+    return this.get<IHome>(`${API_CONFIG.BASE_URL}/${API_CONFIG.HOME}?lang=${lang}`);
   }
 }
