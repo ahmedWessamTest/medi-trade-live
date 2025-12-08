30c3873a2806344aaedbc309f52a0012a6f9873a
ahmedWessamTest <ahmedwessam202@gmail.com>
2025-12-08 16:41:29 +0200

    CLS

--- src/app/core/interceptors/loading-interceptor.ts
+++ src/app/core/interceptors/loading-interceptor.ts
@@ -5,7 +5,6 @@ import { finalize } from 'rxjs';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const spinner = inject(NgxSpinnerService);
-
   spinner.show();
   return next(req).pipe(finalize(() => spinner.hide()));
 };
